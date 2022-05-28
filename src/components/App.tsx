d4d43fc50129bb661aca1a29a279f8dfc0f702f2
Simon <simon.vahlbrock@tobit.com>
2022-05-28 19:52:01 +0200

    ✨ Setup project

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+import './app.scss';
+
+const App: FC = () => (
+    <div className="app">
+    </div>
+);
+
+App.displayName = 'App';
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createRoot} from 'react-dom/client';
+import App from './components/App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const render = () => {
+    const container = document.querySelector('#root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    if (container) {
+        const root = createRoot(container);
+
+        root.render(<App/>);
+    } else {
+        // eslint-disable-next-line no-console
+        console.info('root element could not be found');
+    }
+};
+
+render();
