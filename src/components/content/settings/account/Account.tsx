258b86e921d7d7f894f2be574576e10d851b045a
simon <simon@vahlbrock-net.de>
2022-05-30 17:45:40 +0200

    ✨ add animation to account settings

--- src/components/content/settings/account/Account.tsx
+++ src/components/content/settings/account/Account.tsx
@@ -3,6 +3,7 @@ import './account.scss';
 import Card from "../../../shared/card/Card";
 import {useAppSelector} from "../../../../hooks/redux";
 import {selectUser} from "../../../../redux-modules/user/selectors";
+import TapWrapper from "../../../shared/tap-wrapper/TapWrapper";
 
 const Account: FC = () => {
     const user = useAppSelector(selectUser)
@@ -19,8 +20,12 @@ const Account: FC = () => {
                             <div>Logged In as {user?.name}</div>
                         ) : (
                             <>
-                                <div className="account__card__content__sign-in">Sign In</div>
-                                <div className="account__card__content__sign-up">Sign Up</div>
+                                <TapWrapper>
+                                    <div className="account__card__content__sign-in">Sign In</div>
+                                </TapWrapper>
+                                <TapWrapper>
+                                    <div className="account__card__content__sign-up">Sign Up</div>
+                                </TapWrapper>
                             </>
                         )}
                     </div>
