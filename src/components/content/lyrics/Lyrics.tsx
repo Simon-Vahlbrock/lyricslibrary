240c15a1d006160968d26e9f33628a100a61d4ec
simon <simon@vahlbrock-net.de>
2022-05-31 23:37:43 +0200

    🔥 Remove unused import

--- src/components/content/lyrics/Lyrics.tsx
+++ src/components/content/lyrics/Lyrics.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useCallback, useMemo} from "react";
+import React, {FC, useMemo} from "react";
 import {useAppSelector} from "../../../hooks/redux";
 import {selectSelectedSong} from "../../../redux-modules/songs/selectors";
 import "./lyrics.scss"
