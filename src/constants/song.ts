3f398e948e038c1b1136249958c8eb1e60231498
simon <simon@vahlbrock-net.de>
2022-05-31 20:51:04 +0200

    ♻ Refactor dummy songs

---
+++ src/constants/song.ts
@@ -0,0 +1,102 @@
+import {Song} from "../types/songs";
+
+export const songs: Song[] = [
+    {
+        id: 0,
+        title: "Can't Hold Us",
+        creator: 'Macklemore & Ryan Lewis',
+        lyrics: 'Hey, hey, hey',
+        isFavourite: true
+    },
+    {
+        id: 1,
+        title: "Let It Be",
+        creator: 'The Beatles',
+        lyrics: 'When I find myself in times of trouble',
+        isFavourite: true
+    },
+    {
+        id: 2,
+        title: "Stan",
+        creator: 'Eminem',
+        lyrics: "My tea's gone cold, I'm wondering why I",
+        isFavourite: true
+    },
+    {
+        id: 3,
+        title: "Take Me to Church",
+        creator: 'Hozier',
+        lyrics: "My lover's got humor",
+        isFavourite: true
+    },
+    {
+        id: 4,
+        title: "House Of Memories",
+        creator: 'Panic! At The Disco',
+        lyrics: "If you're a lover, you should know",
+        isFavourite: true
+    },
+    {
+        id: 5,
+        title: "Feeling Good",
+        creator: 'Michael Bublé',
+        lyrics: "Birds flying high",
+        isFavourite: true
+    },
+    {
+        id: 6,
+        title: "Rolling in the Deep",
+        creator: 'Adele',
+        lyrics: "There's a fire starting in my heart",
+        isFavourite: true
+    },
+    {
+        id: 7,
+        title: "Ignite",
+        creator: 'League Of Legends',
+        lyrics: "There's a fire that burns inside",
+        isFavourite: true
+    },
+    {
+        id: 8,
+        title: "The Hanging Tree",
+        creator: 'James Newton Howard',
+        lyrics: "Are you, are you comin' to the tree?",
+        isFavourite: true
+    },
+    {
+        id: 9,
+        title: "The Hanging Tree",
+        creator: 'James Newton Howard',
+        lyrics: "Are you, are you comin' to the tree?",
+        isFavourite: true
+    },
+    {
+        id: 10,
+        title: "My Master My Lord",
+        creator: 'Yukari Tamura',
+        lyrics: "Michi ni nemuru kanbi na maryoku kyoumi wa tsuki masen",
+        isFavourite: true
+    },
+    {
+        id: 11,
+        title: "Arcade",
+        creator: 'Duncan Laurence',
+        lyrics: "A broken heart is all that's left",
+        isFavourite: true
+    },
+    {
+        id: 12,
+        title: "Miss Wanna-Die",
+        creator: 'JubyPhonic',
+        lyrics: "Awake, I opened my eyes to see",
+        isFavourite: true
+    },
+    {
+        id: 13,
+        title: "Something Just Like This",
+        creator: 'Coldplay, The Chainsmoker',
+        lyrics: "I've been reading books of old",
+        isFavourite: true
+    },
+]
--- src/redux-modules/songs/slice.ts
+++ src/redux-modules/songs/slice.ts
@@ -1,5 +1,6 @@
 import {Song} from "../../types/songs";
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {songs} from "../../constants/song";
 
 export interface SongsState {
     songsForSearch?: Song[];
@@ -8,23 +9,7 @@ export interface SongsState {
 }
 
 const initialState: SongsState = {
-    songsForSearch: [
-        {id: 0, creator: 'Simon', lyrics: 'Hey, hey', title: 'Der Profi', isFavourite: true},
-        {id: 1, creator: 'Jens', lyrics: 'Der Weg ist toll', title: 'Der Weg', isFavourite: false},
-        {id: 2, creator: 'Mika', lyrics: 'Ups tada', title: 'Magic', isFavourite: true},
-        {id: 3, creator: 'Reika', lyrics: 'Whatever makes you laught', title: 'Have Fun', isFavourite: false},
-        {id: 4, creator: 'Rees', lyrics: 'Hello There der Profi ist da', title: 'Heute in aller Frische', isFavourite: false},
-        {id: 5, creator: 'Felix', lyrics: 'Hey, hey', title: 'Lieder', isFavourite: false},
-        {id: 6, creator: 'Jan', lyrics: 'Der Weg ist toll', title: 'Das Leben', isFavourite: true},
-        {id: 7, creator: 'Ernst', lyrics: 'Ups tada', title: 'Freude', isFavourite: false},
-        {id: 8, creator: 'Horst', lyrics: 'Whatever makes you laught', title: 'Gerne wieder', isFavourite: true},
-        {id: 9, creator: 'Bella', lyrics: 'Hello There der Profi ist da', title: 'Leben lassen', isFavourite: false},
-        {id: 10, creator: 'Lisa', lyrics: 'Hey, hey', title: 'Heier', isFavourite: false},
-        {id: 11, creator: 'Bart', lyrics: 'Der Weg ist toll', title: 'Der lebende Wicht', isFavourite: true},
-        {id: 12, creator: 'Falka', lyrics: 'Ups tada', title: 'Magic', isFavourite: false},
-        {id: 13, creator: 'Frederike', lyrics: 'Whatever makes you laught', title: 'Immer der Gleiche', isFavourite: true},
-        {id: 14, creator: 'Julian', lyrics: 'Hello There der Profi ist da', title: 'Ein Hoch', isFavourite: true},
-    ]
+    songsForSearch: songs
 };
 
 const slice = createSlice({
